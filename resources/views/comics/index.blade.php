b8095b00121a5c28623b364605fcf6f4a5af0c7e
eltonanit <elton.ani@gmail.com>
2024-10-03 16:32:42 +0200

    [UPD]added controller

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="jumbotron">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                
+
+            </div>
+        </div>
+    </div>
+</div>
+<div id="comics-container">
+    <div class="container">
+        <div class="row">
+        <div class="col-12">
+            
+        </div>
+        </div>
+         
+        <div class="row">
+             
+        </div>
+    </div>
+</div>
+
+@endsection
