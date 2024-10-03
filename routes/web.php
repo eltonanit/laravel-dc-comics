9c4be645097d1aba6599ae13cd53e6e62c6c35f7
eltonanit <elton.ani@gmail.com>
2024-10-03 10:54:46 +0200

    [UPD] Added web app

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+     $comics = config ('db');
+      
+    return view('home', compact('comics') );
+})-> name('homepage');
