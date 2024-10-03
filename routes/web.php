8ae2f19e37f02d67032734487553cfd5d178ed66
eltonanit <elton.ani@gmail.com>
2024-10-03 16:32:17 +0200

    [UPD]Added route comics - controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ComicController as ComicController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
-     $comics = config ('db');
       
-    return view('home', compact('comics') );
+    return view('home');
 })-> name('homepage');
+
+Route::resource('/comics', ComicController::class);
