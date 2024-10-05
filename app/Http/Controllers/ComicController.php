bbf64c52291d3d8090a3cb05ad67d417a4e74c19
eltonanit <elton.ani@gmail.com>
2024-10-05 13:10:51 +0200

    [UPD] added edit in comic controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -56,9 +56,10 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Comic $comic )
     {
-        //
+          return view ('comics.esit', compact('comic'));
+
     }
 
     /**
